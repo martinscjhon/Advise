aaa10b328ab2cd78a0433e173fd3569407b682de
jhonatanMartins <martins.cjhon@gmail.com>
2024-02-21 03:36:26 -0300

    ajustando config

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,10 +1,12 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { ApplicationConfig, mergeApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering()
+    provideServerRendering(),
+    provideHttpClient(withFetch())
   ]
 };
 
